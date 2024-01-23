c5387c95ee06dae39ae1711c2ad64a7c7c61681b
minh-science <156048292+minh-science@users.noreply.github.com>
2024-01-22 21:26:12 -0800

    bfs setup

---
+++ BFS.py
@@ -0,0 +1,5 @@
+from search import graph #graph.py imports networkx
+
+
+
+graph.Graph(filename= "data/tiny_network.adjlist").bfs(start= "Martin Kampmann")
--- search/graph.py
+++ search/graph.py
@@ -10,8 +10,26 @@ class Graph:
         """
         Initialization of graph object 
         """
-        self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+        self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";") # DIRECTED GRAPH!!! no actual direction
 
+    # def f_neighbors(self, start):
+        # print( [i for i in self.graph.nodes()] )
+        # print(type(self.graph))
+        # n_iter = self.graph.successors(start)
+        # print( next(n_iter)) 
+        # for i in self.graph.nodes(): 
+            # print(i)
+            # if i == start:
+                # print("look here", self.graph.nodes(i))
+                # n_dict = next( self.graph.adjacency() )
+        # for i, n_dict in self.graph.adjacency():
+        #     # print(i)
+        #     if i == start:
+        #     #     # print(self.graph.nodes())
+        #         print(f"intermediate nodes of {i}:", n_dict)
+        # print(self.graph.nodes())
+        # return 
+    
     def bfs(self, start, end=None):
         """
         TODO: write a method that performs a breadth first traversal and pathfinding on graph G
@@ -21,8 +39,55 @@ class Graph:
         * If there is an end node input and a path does not exist, return None
 
         """
+        Q = []
+        visited = []
+        Q.append(start)
+        visited.append(start)
+        N = []
+        # print("self.graph:", self.graph.nodes())
+
+        # maybe try using successors? is a generator though
+        # print(f"successors of start (node: {start}):", [i for i in self.graph.successors(start)] )
+        while len(Q) > 0:
+            # print("len Q:", len(Q) ) 
+            v = Q.pop()
+            # print("this is v", v) 
+            N = []
+            ed = self.graph.edges(nbunch = v) #neighbits
+            print(f"edges of v: {v}:", ed)
+            N = [i[1] for i in ed] 
+            # i = next(self.graph.successors(v))
+
+            # print(type(i), i )
+            # print(type(N), N)
+            # N_tuple = ()
+            # if i not in N_tuple: 
+            #     N_tuple + (next(self.graph.successors(v) ))
+            # if i not in N:
+            #     N.append(next(self.graph.successors(v) ) )
+            #     print(i, N)
+            
+            # print("neighbors of of start", N)
+            # print(self.graph)
+            # # print("this is W", w)
+            for w in N: 
+                if w not in visited: 
+                    visited.append(w)
+                    Q.append(w)
+        print("visited nodes", visited)
         return
 
+# Graph(filename ="../data/tiny_network.adjlist").bfs(start = "Martin Kampmann")
+
+print( Graph(filename= "../data/tiny_network.adjlist").bfs(start= "Martin Kampmann") )
+
+# print( Graph(filename= "../data/citation_network.adjlist").bfs(start= "Martin Kampmann") )
+
+# L = ["a", 'b', 'c']
+# print(L)
+# for i in range(0, len(L)):
+#     print(L.pop())
+# print(L)
 
 
-
+# try doing diksthras or whateber
